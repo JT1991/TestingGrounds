50f6e38b4c20cf8b09dc015d59ef9e52aa787130
josh thorn <joshthorn91@gmail.com>
2017-06-14 14:25:21 +0100

    11. Reading blackboard data from C++

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -1,9 +1,12 @@
 // Copyright Thorn Productions Inc.
 
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Ai in C++"));
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent(); 
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint index %f"), Index);
 	return EBTNodeResult::Succeeded;
 }
