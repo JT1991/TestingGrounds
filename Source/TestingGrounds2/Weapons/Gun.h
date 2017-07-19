3feb5d443980743ce766a965762da92f70ed5e97
josh thorn <joshthorn91@gmail.com>
2017-07-19 13:41:03 +0100

    40
    Configuring gun.

--- Source/TestingGrounds2/Weapons/Gun.h
+++ Source/TestingGrounds2/Weapons/Gun.h
@@ -15,6 +15,9 @@ public:
 	// Sets default values for this actor's properties
 	AGun();
 
+	/** Fires a projectile. */
+	void OnFire();
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 	
@@ -47,9 +50,4 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
 	class UAnimInstance* AnimInstance;
-		
-protected:
-
-	/** Fires a projectile. */
-	void OnFire();
 };
