343fd889d845cc0b0e7626f0c9713f9910c2e762
josh thorn <joshthorn91@gmail.com>
2017-07-19 16:31:10 +0100

    43.
    Added gun mesh from C++ to guard.

--- Source/TestingGrounds2/TestingGrounds2HUD.cpp
+++ Source/TestingGrounds2/TestingGrounds2HUD.cpp
@@ -9,7 +9,7 @@
 ATestingGrounds2HUD::ATestingGrounds2HUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshiarTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshiarTexObj(TEXT("/Game/Static/Player/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshiarTexObj.Object;
 }
 
